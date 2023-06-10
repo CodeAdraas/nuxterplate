0f350d4f78d27e59868d4df79c421464fce4dfa5
CodeAdraas <davidachoendov@hotmail.com>
2023-06-10 10:23:08 +0200

    Add alignment for slider - bump versions

--- src/components/util-slider/component.vue
+++ src/components/util-slider/component.vue
@@ -1,29 +1,41 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, type Ref } from 'vue'
+import { ref, computed, onMounted, onBeforeUnmount, type Ref } from 'vue'
 import { Autoplay, Swiper } from 'swiper'
+
+interface Emits {
+    (e: 'swiper', instance: Swiper)
+}
 
 interface Props {
     slidesPerView?: number
     spaceBetween?: number
     autoplay?: number | boolean
     loop?: boolean
+    align?: 'start' | 'center' | 'end'
 }
 
-interface Emits {
-    (e: 'swiper', instance: Swiper)
-}
-
+const emit = defineEmits<Emits>()
 const prop = withDefaults(defineProps<Props>(), {
     slidesPerView: 1,
     spaceBetween: 0,
     autoplay: 5000,
-    loop: true
+    loop: true,
+    align: 'start'
 })
-const emit = defineEmits<Emits>()
 
 const swiperEl = ref()
 const swiperWrapper = ref()
 const swiperController: Ref<Swiper | undefined> = ref()
+const getAlignment = computed(() => {
+switch(prop.align) {
+    case 'start':
+        return 'flex-start'
+    case 'center':
+        return 'flex-end'
+    case 'end':
+        return 'flex-end'
+}
+})
 
 onMounted(() => {
     for (let slide of swiperWrapper.value.children) {
@@ -58,6 +70,7 @@ onBeforeUnmount(() => swiperController.value?.destroy())
         <div
             ref="swiperWrapper"
             class="swiper-wrapper"
+            :style="{alignItems: getAlignment}"
         >
             <slot />
         </div>
